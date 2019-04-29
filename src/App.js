c129848d24886dbbe077ece298b756a7c7bc8998
Michael <welsh.11311@gmail.com>
2019-04-28 20:17:25 -0400

    trying something new

--- src/App.js
+++ src/App.js
@@ -25,8 +25,17 @@ class App extends Component {
       female: true
     });
   }
-  // Add random npc function//
+  dropDown(){
+    var x = document.getElementById("Submenu");
+    if (x.className.indexOf("Show-Submenu") == -1) {
+      x.className += ' Show-Submenu';
+    }else{
+      x.className = x.className.replace(" Show-Submenu"," ");
+    }
 
+  }
+  // Add random npc function//
+  
   render() {
     return (
       
@@ -38,39 +47,14 @@ class App extends Component {
         </div>
         <div className="Menu">2</div> 
         <div className="Left-column">
-          <li>
-            <ul className="Button-column">
-              <li><button className="Buttons Level mainmenubtn"> Level</button></li> {/*used to set Hit points, Prof modifier, features, Spells*/}
-              <div class="dropdown-child">
-                <a href="http://wwww.yourdonain.com/page1.html">Child menu 1</a>
-              </div>
-              <li><button className="Buttons Gender"> Gender</button></li> {/* for role-playing and name choice*/}
-              <li><button className="Buttons Race"> Race</button></li> 
-              <li><button className="Buttons Background">Background</button></li>
-              <li><button className="Buttons Ability-scores">Ability Scores</button></li> {/*Ability modifiers are calculated from these. calculated by the sum of 4d6 - the lowest d6*/}
-              <li><button className="Buttons Saving-throws">Saving Throws</button></li>
-              <li><button className="Buttons Skill-scores">Skill Scores</button></li>
-              <li><button className="Buttons Proficiencies">Proficiencies</button></li>
-              <li><button className="Buttons Language">Languages</button></li>
-              <li><button className="Buttons Equipment">Equipment</button></li>
-              <li><button className="Buttons Spellcasting">Spellcasting</button></li> {/* need to put a list together to choose from*/}
-              <li><button className="Buttons Hit-points">Hit Points</button></li>
-              <li><button className="Buttons Armor-class">Armor Class</button></li>
-              <li><button className="Buttons Traits">Traits</button></li>
-              <li><button className="Buttons Name">Name</button></li>
-              <li><button className="Buttons Alignment">Alignment</button></li>
-            </ul> 
-          </li>
+         <button onClick id="Random-Npc" className="Buttons"> Generate Random Npc
+         </button>
+          <ul id="Submenu" className="Generate-Random-Submenu">
+            <li>Generates completly random Npc</li>
+            <li><button className="Submenu-Buttons">Generate</button></li></ul>
         </div>
-        <div className="Npc-sheet dropdown">4 
-        <div class="dropdown">
-          <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-          <div id="myDropdown" class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div> 
+        <div className="Npc-sheet">4 
+          
         </div>
       </div>
     );
